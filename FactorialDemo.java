2e535c34b5a964107d0a9fe1b8b1e9d87c8971e6
Priyaram <48385836+priyaram2021@users.noreply.github.com>
2022-11-28 15:32:53 +0530

    Add files via upload

---
+++ FactorialDemo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class FactorialDemo
+{
+  public static void main(String args[])
+  {
+    Scanner input = new Scanner(System.in);
+	System.out.println("Enter the number:");
+	int number = input.nextInt();
+	int fact = 1;
+	for (int i=1; i<=number; i++)
+	{
+		fact = fact*i;
+    }
+	System.out.println("Factorial of " + number+"is" +fact);
+  }
+}  
